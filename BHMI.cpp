673fa6ac089420ac76eab12fa3153f888f59adf0
SC-One <heydar.mahmoodi75@gmail.com>
2022-12-27 09:00:04 +0330

    added time and date

--- BHMI.cpp
+++ BHMI.cpp
@@ -1,15 +1,22 @@
 #include "BHMI.h"
+
+#include <QDateTime>
+
 #include "./ui_BHMI.h"
 
-BHMI::BHMI(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::BHMI)
-{
-    ui->setupUi(this);
+BHMI::BHMI(QWidget *parent) : QMainWindow(parent), ui(new Ui::BHMI) {
+  ui->setupUi(this);
+  updateDateTime();
+  _timeUpdater.setInterval(1000);
+  connect(&_timeUpdater, &QTimer::timeout, this, &BHMI::updateDateTime);
+  _timeUpdater.start();
 }
 
-BHMI::~BHMI()
-{
-    delete ui;
-}
+BHMI::~BHMI() { delete ui; }
 
+void BHMI::updateDateTime() {
+  auto currentDateTime = QDateTime::currentDateTime();
+  ui->timeLbl->setText(currentDateTime.time().toString());
+  ui->dateLbl->setText(currentDateTime.date().toString());
+  ui->dayLbl->setText(currentDateTime.toString("dddd"));
+}
--- BHMI.h
+++ BHMI.h
@@ -2,20 +2,26 @@
 #define BHMI_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class BHMI; }
+namespace Ui {
+class BHMI;
+}
 QT_END_NAMESPACE
 
-class BHMI : public QMainWindow
-{
-    Q_OBJECT
+class BHMI : public QMainWindow {
+  Q_OBJECT
 
-public:
-    BHMI(QWidget *parent = nullptr);
-    ~BHMI();
+ public:
+  BHMI(QWidget *parent = nullptr);
+  ~BHMI();
 
-private:
-    Ui::BHMI *ui;
+ private slots:
+  void updateDateTime();
+
+ private:
+  Ui::BHMI *ui;
+  QTimer _timeUpdater;
 };
-#endif // BHMI_H
+#endif  // BHMI_H
